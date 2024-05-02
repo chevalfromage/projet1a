7426e1b4f421fa4c859dbc616e8419ce788e20f5
MICHA Nicolas <Nicolas.Micha@bordeaux-inp.fr>
2024-05-02 11:49:00 +0200

    copy pas fini

--- lectecr.c
+++ lectecr.c
@@ -5,17 +5,34 @@
 
 
 
-/*
-FILE* copy(char* name,char* format){
-    int n = strlen(name);
-    for (int i = 0,i !='\0',i++);
+FILE* copy(char* name_entree, char* name_sortie){
+    int n_entree = strlen(name_entree);
+    int n_sortie = strlen(name_sortie);
+    char* format_entree;
+    char* format_sortie;
+    char** liste_format = {"ppm","jpg"};
+    //recuperation du format
+    for (int i = n-1,i>n-4,i--){
+        format_entree[n-1-i] = name_entree[i];
+        format_sortie[n-1-i] = name_sortie[i];
+    }
+    //verification si format valide 
+    for (int i=0;i<3;i++){
+        if (format_entree[i]!= liste_format[0][i] || format_entree[i]!= liste_format[1][i]){
+            printf("L'image rentree n'a pas un bon format\n");
+        }   re
+    }
+    //comparaison des 2 formats
+    for (int i=0;i<3;i++){
+        if (format_entree[0]== liste_format_sortie[0]){
+            name_sortie
+        }
+    
 }
 
-*/
+
 
 
 int main(){
-
-    printf("%d\n",strlen("hdget"));
     return 0;
 }
