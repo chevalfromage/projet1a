1077a9bb4e3f62aecd6d404ac2a593cd20b53b47
MICHA Nicolas <Nicolas.Micha@bordeaux-inp.fr>
2024-05-02 11:43:19 +0200

    copy pas fini

---
+++ lectecr.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+
+
+/*
+FILE* copy(char* name,char* format){
+    int n = strlen(name);
+    for (int i = 0,i !='\0',i++);
+}
+
+*/
+
+
+int main(){
+
+    printf("%d\n",strlen("hdget"));
+    return 0;
+}
