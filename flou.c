5bdf7bebfcfb777cf32cc83b9078aa619f9d6b73
Molkiamateur <nicolasmicha9@gmail.com>
2024-05-26 17:09:50 +0200

    marre

--- flou.c
+++ flou.c
@@ -3,181 +3,74 @@
 #include "flou.h"
 
 void flou(char* imageppm,char* out,int s){
-    FILE* f = fopen(imageppm,"r");
     picture pic = read_pic(imageppm);
-    static int tab[pic.width][pic.height][3];
-    static float new_tab[pic.width][pic.height][3];
+    printf("oui\n");
+    
     picture new_pic;
     new_pic.height = pic.height;
     new_pic.width = pic.width;
-    int nb_pixel_coin = 2*s+2;
-    int nb_pixel_bord = (2*s+1)*(2*s+1)-s*(2*s+1);
+    new_pic.pixels =(color*)malloc(pic.width * pic.height * sizeof(color));
+    //int nb_pixel_coin = 2*s+2;
+    //int nb_pixel_bord = (2*s+1)*(2*s+1)-s*(2*s+1);
     int nb_pixel_milieu = (2*s+1)*(2*s+1);
     int moy_red,moy_green,moy_blue;
-    for (int i =0;i< pic.height;i++){
+    color **tab = (color**)malloc(pic.height * sizeof(color*));
+    color **new_tab = (color**)malloc(pic.height * sizeof(color*));
+    for (int i = 0; i < pic.height; i++) {
+        tab[i] = (color*)malloc(pic.width * sizeof(color));
+        new_tab[i] = (color*)malloc(pic.width * sizeof(color));
         for (int j =0;j<pic.width;j++){
             tab[i][j] = pic.pixels[i*pic.width+j];
         }
     }
+    
     for (int i =0;i< pic.height;i++){
-        for (int j =0;j<pic.width;j++){     
-            int s_red,s_green,s_blue;
-            if (i==0){
-                if (j ==0){
-                    for (int k=i;k<i+s+1;k++){
-                        for (int l = j;l<j+s+1;l++){
-                            s_red += tab[k][l][0];
-                            s_green += tab[k][l][1];
-                            s_blue += tab[k][l][2];
+        for (int j =0;j<pic.width;j++){
+            int s_red =0;
+            int s_green = 0;
+            int s_blue=0;
+            printf("%d,%d,%d\n",pic.pixels[i*pic.width+j].red,pic.pixels[i*pic.width+j].green,pic.pixels[i*pic.width+j].blue);
+            for (int k= i-s;k<i+s+1;k++){
+                if (k<pic.height && k>=0){
+                    
+                    for (int l=j-s;l<j+s+1;l++){
+                        if( l < pic.width && l >=0){
+                            
+                            s_red += tab[k][l].red;
+                            s_green += tab[k][l].green;
+                            s_blue += tab[k][l].blue;
                         }
-                    }
-                    moy_red = s_red / nb_pixel_coin;
-                    moy_green = s_green/nb_pixel_coin;
-                    moy_blue = s_blue/nb_pixel_coin;
-                    new_tab[i][j][0] = moy_red;
-                    new_tab[i][j][1] = moy_green;
-                    new_tab[i][j][2] = moy_blue; 
+                    }      
+                        
+                }
+            }
+            
+            moy_red = s_red / nb_pixel_milieu;
+            moy_green = s_green/nb_pixel_milieu;
+            moy_blue = s_blue/nb_pixel_milieu;
+            new_tab[i][j].red = moy_red;
+            new_tab[i][j].green = moy_green;
+            new_tab[i][j].blue = moy_blue;
+            //printf("%d,%d,%d\n",new_tab[i][j].red,new_tab[i][j].green,new_tab[i][j].blue);
 
-                }
-                if (j==pic.width-1){
-                    for (int k=i;k<i+s+1;k++){
-                        for (int l = j-s;l<j+1;l++){
-                            s_red += tab[k][l][0];
-                            s_green += tab[k][l][1];
-                            s_blue += tab[k][l][2];
-                        }
-                    }
-                    moy_red = s_red / nb_pixel_coin;
-                    moy_green = s_green/nb_pixel_coin;
-                    moy_blue = s_blue/nb_pixel_coin;
-                    new_tab[i][j][0] = moy_red;
-                    new_tab[i][j][1] = moy_green;
-                    new_tab[i][j][2] = moy_blue;
-                }
-                else {
-                    
-                    for (int k=i;k<i+s+1;k++){
-                        for (int l = j-s;l<j+s+1;l++){
-                            s_red += tab[k][l][0];
-                            s_green += tab[k][l][1];
-                            s_blue += tab[k][l][2];
-                            
-                        }
-                    }
-                    moy_red = s_red / nb_pixel_bord;
-                    moy_green = s_green/nb_pixel_bord;
-                    moy_blue = s_blue/nb_pixel_bord;
-                    new_tab[i][j][0] = moy_red;
-                    new_tab[i][j][1] = moy_green;
-                    new_tab[i][j][2] = moy_blue;
-                }
-            }
-            if (i==pic.height-1){
-                if (j ==0){
-                    
-                    for (int k=i-s;k<i+1;k++){
-                        for (int l = j;l<j+s+1;l++){
-                            s_red += tab[k][l][0];
-                            s_green += tab[k][l][1];
-                            s_blue += tab[k][l][2];
-                            
-                        }
-                    }
-                    moy_red = s_red / nb_pixel_coin;
-                    moy_green = s_green/nb_pixel_coin;
-                    moy_blue = s_blue/nb_pixel_coin;
-                    new_tab[i][j][0] = moy_red;
-                    new_tab[i][j][1] = moy_green;
-                    new_tab[i][j][2] = moy_blue; 
-
-                }
-                if (j==pic.width-1){
-                    
-                    for (int k=i-s;k<i+1;k++){
-                        for (int l = j-s;l<j+1;l++){
-                            s_red += tab[k][l][0];
-                            s_green += tab[k][l][1];
-                            s_blue += tab[k][l][2];
-                            
-                        }
-                    }
-                    moy_red = s_red / nb_pixel_coin;
-                    moy_green = s_green/nb_pixel_coin;
-                    moy_blue = s_blue/nb_pixel_coin;
-                    new_tab[i][j][0] = moy_red;
-                    new_tab[i][j][1] = moy_green;
-                    new_tab[i][j][2] = moy_blue;
-                }
-                else {
-                    
-                    for (int k=i-s;k<i+1;k++){
-                        for (int l = j-s;l<j+s+1;l++){
-                            s_red += tab[k][l][0];
-                            s_green += tab[k][l][1];
-                            s_blue += tab[k][l][2];
-                            
-                        }
-                    }
-                    moy_red = s_red / nb_pixel_bord;
-                    moy_green = s_green/nb_pixel_bord;
-                    moy_blue = s_blue/nb_pixel_bord;
-                    new_tab[i][j][0] = moy_red;
-                    new_tab[i][j][1] = moy_green;
-                    new_tab[i][j][2] = moy_blue;
-                }
-            }
-            if (j==0 && i!=0 && i!=pic.height-1){
-                
-                    for (int k=i-s;k<i+s+1;k++){
-                        for (int l = j;l<j+s+1;l++){
-                            s_red += tab[k][l][0];
-                            s_green += tab[k][l][1];
-                            s_blue += tab[k][l][2];
-                            
-                        }
-                    }
-                    moy_red = s_red / nb_pixel_bord;
-                    moy_green = s_green/nb_pixel_bord;
-                    moy_blue = s_blue/nb_pixel_bord;
-                    new_tab[i][j][0] = moy_red;
-                    new_tab[i][j][1] = moy_green;
-                    new_tab[i][j][2] = moy_blue;
-            }
-            if (j==pic.width-1 && i!=0 && i != pic.height-1){
-                
-                    for (int k=i-s;k<i+s+1;k++){
-                        for (int l = j-s;l<j+1;l++){
-                            s_red += tab[k][l][0];
-                            s_green += tab[k][l][1];
-                            s_blue += tab[k][l][2];
-                            
-                        }
-                    }
-                    moy_red = s_red / nb_pixel_bord;
-                    moy_green = s_green/nb_pixel_bord;
-                    moy_blue = s_blue/nb_pixel_bord;
-                    new_tab[i][j][0] = moy_red;
-                    new_tab[i][j][1] = moy_green;
-                    new_tab[i][j][2] = moy_blue;
-            }            
-            else {
-                for (int k= i-s;k<i+s+1<k++){
-                    for (int l=j-s;l<j+s+1<l++){
-                    s_red += tab[k][l][0];
-                    s_green += tab[k][l][1];
-                    s_blue += tab[k][l][2];
-
-                    }
-                }
-                moy_red = s_red / nb_pixel_milieu;
-                moy_green = s_green/nb_pixel_milieu;
-                moy_blue = s_blue/nb_pixel_milieu;
-                new_tab[i][j][0] = moy_red;
-                new_tab[i][j][1] = moy_green;
-                new_tab[i][j][2] = moy_blue; 
-            }
-            new_pic.pixels[i*new_pic.width+j] = new_tab[i][j];
+            new_pic.pixels[i*new_pic.width+j].red = new_tab[i][j].red;
+            new_pic.pixels[i*new_pic.width+j].green = new_tab[i][j].green;
+            new_pic.pixels[i*new_pic.width+j].blue = new_tab[i][j].blue;
+            //printf("%d,%d,%d\n",new_pic.pixels[i*new_pic.width+j].red,new_pic.pixels[i*new_pic.width+j].green,new_pic.pixels[i*new_pic.width+j].blue);
         }
+                
     }
+    for (int i = 0; i < pic.height; i++) {
+        free(tab[i]);
+        free(new_tab[i]);
+    }
+    
+    free(tab);
+    free(new_tab);
+    
+    free(new_pic.pixels);
+    printf("000i\n");
     save_pic(new_pic,out);
+    printf("000i\n");
+    
 }
